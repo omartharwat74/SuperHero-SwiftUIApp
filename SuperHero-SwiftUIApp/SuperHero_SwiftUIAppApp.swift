6fb464c8e8903737ecc9b7e0420447aa7800e0b7
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2023-05-08 20:06:42 +0300

    Initial Commit

---
+++ SuperHero-SwiftUIApp/SuperHero_SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHero_SwiftUIAppApp.swift
+//  SuperHero-SwiftUIApp
+//
+//  Created by Omar Tharwat on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHero_SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
